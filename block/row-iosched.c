bd574b5ddb84cb9b4e8384f6f3a9f7ece980904c
tuxafgmur <usuario@localhost.localdomain>
2013-04-19 00:15:09 +0200

    Row scheduler, hardcode values research

--- block/row-iosched.c
+++ block/row-iosched.c
@@ -49,27 +49,27 @@ enum row_queue_prio {
 static const bool queue_idling_enabled[] = {
 	true,	/* ROWQ_PRIO_HIGH_READ */
 	true,	/* ROWQ_PRIO_REG_READ */
-	false,	/* ROWQ_PRIO_HIGH_SWRITE */
-	false,	/* ROWQ_PRIO_REG_SWRITE */
-	false,	/* ROWQ_PRIO_REG_WRITE */
-	false,	/* ROWQ_PRIO_LOW_READ */
-	false,	/* ROWQ_PRIO_LOW_SWRITE */
+	true,	/* ROWQ_PRIO_HIGH_SWRITE */
+	true,	/* ROWQ_PRIO_REG_SWRITE */
+	true,	/* ROWQ_PRIO_REG_WRITE */
+	true,	/* ROWQ_PRIO_LOW_READ */
+	true,	/* ROWQ_PRIO_LOW_SWRITE */
 };
 
 /* Default values for row queues quantums in each dispatch cycle */
 static const int queue_quantum[] = {
-	100,	/* ROWQ_PRIO_HIGH_READ */
-	100,	/* ROWQ_PRIO_REG_READ */
-	2,	/* ROWQ_PRIO_HIGH_SWRITE */
-	1,	/* ROWQ_PRIO_REG_SWRITE */
-	1,	/* ROWQ_PRIO_REG_WRITE */
-	1,	/* ROWQ_PRIO_LOW_READ */
-	1	/* ROWQ_PRIO_LOW_SWRITE */
+	10,	/* ROWQ_PRIO_HIGH_READ */
+	75,	/* ROWQ_PRIO_REG_READ */
+	5,	/* ROWQ_PRIO_HIGH_SWRITE */
+	4,	/* ROWQ_PRIO_REG_SWRITE */
+	4,	/* ROWQ_PRIO_REG_WRITE */
+	3,	/* ROWQ_PRIO_LOW_READ */
+	12	/* ROWQ_PRIO_LOW_SWRITE */
 };
 
 /* Default values for idling on read queues */
-#define ROW_IDLE_TIME_MSEC 5	/* msec */
-#define ROW_READ_FREQ_MSEC 20	/* msec */
+#define ROW_IDLE_TIME_MSEC 10	/* msec */
+#define ROW_READ_FREQ_MSEC 25	/* msec */
 
 /**
  * struct rowq_idling_data -  parameters for idling on the queue
@@ -467,9 +467,7 @@ static void row_exit_queue(struct elevator_queue *e)
 	int i;
 
 	for (i = 0; i < ROWQ_MAX_PRIO; i++)
-		BUG_ON(!list_empty(&rd->row_queues[i].rqueue.fifo));
 	(void)cancel_delayed_work_sync(&rd->read_idle.idle_work);
-	BUG_ON(delayed_work_pending(&rd->read_idle.idle_work));
 	destroy_workqueue(rd->read_idle.idle_workqueue);
 	kfree(rd);
 }
@@ -591,23 +589,17 @@ static ssize_t __FUNC(struct elevator_queue *e,				\
 STORE_FUNCTION(row_hp_read_quantum_store,
 &rowd->row_queues[ROWQ_PRIO_HIGH_READ].disp_quantum, 1, INT_MAX, 0);
 STORE_FUNCTION(row_rp_read_quantum_store,
-			&rowd->row_queues[ROWQ_PRIO_REG_READ].disp_quantum,
-			1, INT_MAX, 0);
+			&rowd->row_queues[ROWQ_PRIO_REG_READ].disp_quantum, 1, INT_MAX, 0);
 STORE_FUNCTION(row_hp_swrite_quantum_store,
-			&rowd->row_queues[ROWQ_PRIO_HIGH_SWRITE].disp_quantum,
-			1, INT_MAX, 0);
+			&rowd->row_queues[ROWQ_PRIO_HIGH_SWRITE].disp_quantum, 1, INT_MAX, 0);
 STORE_FUNCTION(row_rp_swrite_quantum_store,
-			&rowd->row_queues[ROWQ_PRIO_REG_SWRITE].disp_quantum,
-			1, INT_MAX, 0);
+			&rowd->row_queues[ROWQ_PRIO_REG_SWRITE].disp_quantum, 1, INT_MAX, 0);
 STORE_FUNCTION(row_rp_write_quantum_store,
-			&rowd->row_queues[ROWQ_PRIO_REG_WRITE].disp_quantum,
-			1, INT_MAX, 0);
+			&rowd->row_queues[ROWQ_PRIO_REG_WRITE].disp_quantum, 1, INT_MAX, 0);
 STORE_FUNCTION(row_lp_read_quantum_store,
-			&rowd->row_queues[ROWQ_PRIO_LOW_READ].disp_quantum,
-			1, INT_MAX, 0);
+			&rowd->row_queues[ROWQ_PRIO_LOW_READ].disp_quantum, 1, INT_MAX, 0);
 STORE_FUNCTION(row_lp_swrite_quantum_store,
-			&rowd->row_queues[ROWQ_PRIO_LOW_SWRITE].disp_quantum,
-			1, INT_MAX, 1);
+			&rowd->row_queues[ROWQ_PRIO_LOW_SWRITE].disp_quantum, 1, INT_MAX, 1);
 STORE_FUNCTION(row_read_idle_store, &rowd->read_idle.idle_time, 1, INT_MAX, 1);
 STORE_FUNCTION(row_read_idle_freq_store, &rowd->read_idle.freq, 1, INT_MAX, 0);
 
@@ -638,8 +630,8 @@ static struct elevator_type iosched_row = {
 		.elevator_former_req_fn		= elv_rb_former_request,
 		.elevator_latter_req_fn		= elv_rb_latter_request,
 		.elevator_set_req_fn		= row_set_request,
-		.elevator_init_fn		= row_init_queue,
-		.elevator_exit_fn		= row_exit_queue,
+		.elevator_init_fn			= row_init_queue,
+		.elevator_exit_fn			= row_exit_queue,
 	},
 
 	.elevator_attrs = row_attrs,
