5cbc45ecf5dc5cc739cdcef0f44af07a02936aee
tuxafgmur <usuario@localhost.localdomain>
2013-04-21 22:52:01 +0200

    Filesystem patch

--- fs/proc/array.c
+++ fs/proc/array.c
@@ -321,10 +321,9 @@ static inline void task_cap(struct seq_file *m, struct task_struct *p)
 static inline void task_context_switch_counts(struct seq_file *m,
 						struct task_struct *p)
 {
-	seq_printf(m,	"voluntary_ctxt_switches:\t%lu\n"
+	seq_printf(m, "voluntary_ctxt_switches:\t%lu\n"
 			"nonvoluntary_ctxt_switches:\t%lu\n",
-			p->nvcsw,
-			p->nivcsw);
+			p->nvcsw, p->nivcsw);
 }
 
 static void task_cpus_allowed(struct seq_file *m, struct task_struct *task)
@@ -337,6 +336,12 @@ static void task_cpus_allowed(struct seq_file *m, struct task_struct *task)
 	seq_putc(m, '\n');
 }
 
+static void task_vpid(struct seq_file *m, struct task_struct *task)
+{
+  struct pid_namespace *ns = task_active_pid_ns(task);
+  seq_printf(m, "Vpid:\t%d\n", ns ? task_pid_nr_ns(task, ns) : 0);
+}
+ 
 int proc_pid_status(struct seq_file *m, struct pid_namespace *ns,
 			struct pid *pid, struct task_struct *task)
 {
@@ -354,6 +359,7 @@ int proc_pid_status(struct seq_file *m, struct pid_namespace *ns,
 	task_cpus_allowed(m, task);
 	cpuset_task_status_allowed(m, task);
 	task_context_switch_counts(m, task);
+	task_vpid(m, task);
 	return 0;
 }
 
