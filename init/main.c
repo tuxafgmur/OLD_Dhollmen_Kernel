5cf9aefbc239f2db93702ecf5cd8660df39cba32
tuxafgmur <usuario@localhost.localdomain>
2013-06-30 18:48:31 +0200

    Kernel update to 3.0.39

--- init/main.c
+++ init/main.c
@@ -545,9 +545,6 @@ asmlinkage void __init start_kernel(void)
 	early_boot_irqs_disabled = false;
 	local_irq_enable();
 
-	/* Interrupts are enabled now so all GFP allocations are safe. */
-	gfp_allowed_mask = __GFP_BITS_MASK;
-
 	kmem_cache_init_late();
 
 	/*
@@ -789,6 +786,10 @@ static void __init kernel_init_freeable(void)
 	 * Wait until kthreadd is all set-up.
 	 */
 	wait_for_completion(&kthreadd_done);
+
+	/* Now the scheduler is fully set up and can do blocking allocations */
+	gfp_allowed_mask = __GFP_BITS_MASK;
+
 	/*
 	 * init can allocate pages on any node
 	 */
